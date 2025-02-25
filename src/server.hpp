silvermantis7/epilogue

commit a8e29b79763ac6d480ee3dc84c118237ab575a34

Modify connection->read_messages() to return commands.
connection->read_messages() now returns an std::vector of type
epilogue::Command instead of type std::string. The read_messages
function now also directly responds to PING messages.

src/server.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <asio.hpp>
 
+#include "process_messages.hpp"
+
 using asio::ip::tcp;
 
 namespace epilogue
@@ -16,7 +18,7 @@ namespace epilogue
         }
 
         void connect(std::string host, std::string port);
-        std::vector<std::string> read_messages();
+        std::vector<epilogue::Command> read_messages();
         void send_message(std::string message);
         void display_message(const asio::error_code& error,
             std::size_t bytes_transferred);
@@ -45,7 +47,7 @@ namespace epilogue
         tcp::socket socket;
 
         std::vector<std::string> channels = { "*global*" };
-        
+
         std::mutex send_mutex;
 
         // used for when message recieved is too big for read_messages() buffer
@@ -61,7 +63,7 @@ void epilogue::Connection::connect(std::string host, std::string port)
     asio::connect(socket, endpoints);
 }
 
-std::vector<std::string> epilogue::Connection::read_messages()
+std::vector<epilogue::Command> epilogue::Connection::read_messages()
 {
     constexpr unsigned int READ_BUF_SIZE = 512;
 
@@ -90,11 +92,23 @@ std::vector<std::string> epilogue::Connection::read_messages()
 
     std::stringstream ss(buffer_data);
     std::string message;
-    std::vector<std::string> messages;
+    std::vector<epilogue::Command> messages;
 
     while(std::getline(ss, message, '\n'))
     {
-        messages.push_back(message);
+        if (message.back() == '\r')
+        {
+            message.pop_back();
+        }
+
+        epilogue::Command command = epilogue::process_message(message);
+
+        if (command.cmd_id == epilogue::Command_ID::PING)
+        {
+            send_message("PONG " + command.body + "\r\n");
+        }
+
+        messages.push_back(command);
         std::cout << ">>> " << message << "\n";
     }
 
@@ -111,7 +125,7 @@ std::vector<std::string> epilogue::Connection::read_messages()
 void epilogue::Connection::send_message(std::string message)
 {
     std::error_code ignored_error;
-    
+
     send_mutex.lock();
 
     if (!closed_)

src/gui.cpp
@@ -1,6 +1,6 @@
-#include "gui.hpp"
-#include "process_messages.hpp"
 #include <thread>
+
+#include "gui.hpp"
 
 wxIMPLEMENT_APP(gui::Epilogue);
 
@@ -185,26 +185,39 @@ static void gui::receive_messages()
                 break;
             }
 
-            for (std::string message : connection->read_messages())
+            for (epilogue::Command command : connection->read_messages())
             {
-                message.pop_back(); // remove '\r' character
-                epilogue::Command command = epilogue::process_message(message);
-
                 // types of commands to be looged in the message display
                 static const std::vector<epilogue::Command_ID> loggable = {
                     epilogue::Command_ID::PRIVMSG,
                     epilogue::Command_ID::JOIN
                 };
 
-                if (command.cmd_id == epilogue::Command_ID::PING)
+                switch (command.cmd_id)
                 {
-                    connection->send_message("PONG " + command.body + "\r\n");
+                case epilogue::Command_ID::JOIN:
+                    if (command.sender == epilogue::nick)
+                    {
+                        main_frame->join(command.context);
+                        command.body = "joined [" + command.context + "]";
+                        command.context = "*global*";
+                        command.sender = "*.*";
+                    }
+
+                    break;
+
+                default:
+                    break;
                 }
 
                 if (std::find(loggable.begin(), loggable.end(), command.cmd_id)
                     != loggable.end())
                 {
-                    if (command.context == "*none*") break;
+                    if (command.context == "*none*")
+                    {
+                        break;
+                    }
+
                     wxWindow* notebook
                         = gui::main_frame->get_notebook()->GetCurrentPage();
 
@@ -322,23 +335,27 @@ void gui::Connect_Dialog::connect(wxCommandEvent& event)
         connection->send_message("NICK :" + nick + "\r\n");
         connection->send_message("USER " + nick + " 0 * :" + realname + "\r\n");
 
-        std::vector<std::string> received_messages;
+        std::vector<epilogue::Command> received_messages;
 
-        do
+        for(;;)
         {
-            received_messages = connection->read_messages();
+            for (epilogue::Command command : connection->read_messages())
+            {
+                switch (command.cmd_id)
+                {
+                case epilogue::Command_ID::WELCOME:
+                    EndModal(0);
+                    return;
+
+                // case epilogue::Command_ID::1234:
+                //     wxMessageBox(command.body, "", wxOK | wxICON_INFORMAITON);
+                //     break;
+
+                default: break;
+                }
+            }
+
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        }
-        while (received_messages.empty());
-
-        epilogue::Command command;
-        command = epilogue::process_message(received_messages.front());
-
-        if (command.cmd_id != epilogue::Command_ID::WELCOME)
-        {
-            wxMessageBox(command.body, "invalid username",
-                wxOK | wxICON_INFORMATION);
-            return;
         }
     }
     catch (std::exception& e)
@@ -348,8 +365,6 @@ void gui::Connect_Dialog::connect(wxCommandEvent& event)
             wxOK | wxICON_INFORMATION);
         return;
     }
-
-    EndModal(0);
 }
 
 static void gui::update_statusbar(wxStatusBar* statusbar,

src/gui.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <wx/wx.h>
 
 #include <wx/artprov.h>
@@ -22,7 +24,7 @@
 namespace gui
 {
     asio::io_context io_context;
-    ::epilogue::Connection::pointer connection;
+    epilogue::Connection::pointer connection;
     std::thread read_thread;
     std::thread send_thread;
 
@@ -72,7 +74,7 @@ namespace gui
         void send_message(wxCommandEvent& event);
 
         ~Panel();
-        
+
         void log_message(const std::string& sender, const std::string& message);
     };
 
@@ -87,7 +89,7 @@ namespace gui
 
         void on_close(wxCloseEvent& event);
 
-    public:    
+    public:
         Connect_Dialog(wxWindow* parent, wxWindowID id = wxID_ANY,
             const wxString& title = _("connect to server"),
             const wxPoint& pos = wxDefaultPosition,
@@ -109,7 +111,7 @@ namespace gui
 
     static void update_statusbar(wxStatusBar* statusbar,
         std::string* channel_context);
-    
+
     struct Message_Label : public wxStaticText
     {
         Message_Label(wxScrolledWindow* message_display,

src/process_messages.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <unordered_map>
 
 namespace epilogue
@@ -56,14 +58,17 @@ epilogue::Command epilogue::process_message(std::string message)
             command_id = epilogue::Command_ID::WELCOME;
             epilogue::nick = words.at(2);
             break;
+
         case 433:
             command_id = epilogue::Command_ID::NICK_TAKEN;
             command_body = message.substr(message.find(':', 1) + 1);
             break;
+
         case 432:
             command_id = epilogue::Command_ID::ERRONEOUS_NICK;
             command_body = message.substr(message.find(':', 1) + 1);
             break;
+
         default:
             break;
         }
@@ -91,20 +96,11 @@ epilogue::Command epilogue::process_message(std::string message)
     // channel join
     else if (words.at(1) == "JOIN")
     {
-        std::string channel = words.at(2);
-        std::string nick_joined = message.substr(1, message.find('!') - 1);
+        channel_context = words.at(2);
+        sender = message.substr(1, message.find('!') - 1);
 
         command_id = epilogue::Command_ID::JOIN;
-        command_body = channel + " <- " + nick_joined;
-
-        if (nick_joined != nick)
-        {
-            channel_context = channel;
-        }
-        else
-        {
-            gui::main_frame->join(channel);
-        }
+        command_body = channel_context + " <- " + sender;
     }
 
     epilogue::Command command = {

commit cdfe2037f726adbaae86f93f4abe2f1e2911a197

Fix read_overflow.
Fix bug introduced by commit a8e29b79763ac6d480ee3dc84c118237ab575a34,
which processed messages before checking whether they were complete i.e.
not cut off at the end of the read buffer.

src/server.hpp
@@ -92,7 +92,8 @@ std::vector<epilogue::Command> epilogue::Connection::read_messages()
 
     std::stringstream ss(buffer_data);
     std::string message;
-    std::vector<epilogue::Command> messages;
+    std::vector<std::string> messages;
+    std::vector<epilogue::Command> commands;
 
     while(std::getline(ss, message, '\n'))
     {
@@ -101,15 +102,7 @@ std::vector<epilogue::Command> epilogue::Connection::read_messages()
             message.pop_back();
         }
 
-        epilogue::Command command = epilogue::process_message(message);
-
-        if (command.cmd_id == epilogue::Command_ID::PING)
-        {
-            send_message("PONG " + command.body);
-        }
-
-        messages.push_back(command);
-        std::cout << ">>> " << message << "\n";
+        messages.push_back(message);
     }
 
     // if last line overruns buffer
@@ -119,7 +112,20 @@ std::vector<epilogue::Command> epilogue::Connection::read_messages()
         messages.pop_back();
     }
 
-    return messages;
+    for (std::string message : messages)
+    {
+        epilogue::Command command = epilogue::process_message(message);
+
+        if (command.cmd_id == epilogue::Command_ID::PING)
+        {
+            send_message("PONG " + command.body);
+        }
+
+        commands.push_back(command);
+        std::cout << ">>> " << message << "\n";
+    }
+
+    return commands;
 }
 
 void epilogue::Connection::send_message(std::string message)